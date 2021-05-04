32ad09f3d201c0bb3033ed49c9195ebe33b9d327
stefantrinh1 <stefantrinh1.work@gmail.com>
2021-05-04 11:09:01 -0600

    [skip ci]

---
+++ bigAppleSmoothie/gatsby-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-u3JT7IsFQHKL_Y-t96ySmQ',
+        poweredID: 'bzn-u3JT7IsFQHKL_Y-t96ySmQ',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: '', // leave as empty string if not needed
+      },
+    },
+  ],
+}
---
+++ bigAppleSmoothie/src/gatsby-theme-minimal/gatsby-plugin-theme-ui/colors.js
@@ -0,0 +1,7 @@
+export default {
+  text: '#2c2222',
+  background: '#fefefe',
+  primary: '#305d3e',
+  secondary: '#fc923d',
+  light: '#FFF',
+}
