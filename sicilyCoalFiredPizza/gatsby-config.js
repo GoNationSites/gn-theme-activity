51ed713ab20ea88c11b60b66f79e242afc29db6c
stefantrinh1 <stefantrinh1.work@gmail.com>
2021-05-05 08:04:55 -0600

    [skip ci] - adding sicliy pizza

---
+++ sicilyCoalFiredPizza/gatsby-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-MJ4iLOJ3RlWW65wS6nRoEw',
+        poweredID: 'bzn-MJ4iLOJ3RlWW65wS6nRoEw',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: '', // leave as empty string if not needed
+      },
+    },
+  ],
+}
