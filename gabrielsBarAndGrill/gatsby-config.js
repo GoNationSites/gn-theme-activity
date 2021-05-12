9529f709ef08f414f244123ada5e746570faa814
stefantrinh1 <stefantrinh1.work@gmail.com>
2021-05-12 07:40:13 -0600

    [skip ci] - adding gabriels bar to the generator

---
+++ gabrielsBarAndGrill/gatsby-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-y9ikdeb-TQO6cCK8hy9yNQ',
+        poweredID: 'bzn-y9ikdeb-TQO6cCK8hy9yNQ',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: '', // leave as empty string if not needed
+      },
+    },
+  ],
+}
---
+++ gabrielsBarAndGrill/src/gatsby-theme-minimal/components/logo/Logo.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState, useRef } from 'react'
+import { Image, Heading } from 'theme-ui'
+
+export default function Logo({ logoImageId }) {
+  const [scrolled, SetScrolled] = useState(false)
+
+  const scrollRef = useRef()
+  scrollRef.current = scrolled
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const show = window.scrollY > 70
+      if (scrollRef.current !== show) {
+        SetScrolled(show)
+      }
+    }
+
+    document.addEventListener('scroll', handleScroll)
+  }, [])
+
+  return (
+    // <Image
+    //   src={`https://res.cloudinary.com/gonation/${logoImageId}`}
+    //   variant={`logo.${!scrolled ? 'logo' : 'logoScrolled'}`}
+    //   alt='brand'
+    // />
+
+    <Heading as='h1' sx={{ padding: ['1.5rem 1rem', '', '3rem 1rem'] }}>
+      Gabriel's Bar & Grill
+    </Heading>
+  )
+}
