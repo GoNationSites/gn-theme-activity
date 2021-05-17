e9559c299dbfa3a4119e35f4d607253ceda2623c
stefantrinh1 <stefantrinh1.work@gmail.com>
2021-05-17 13:33:44 -0600

    [skip ci] adding thief temp page

---
+++ thief/gatsby-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn--V8HvCSHSx2W4xcxiSrgFA',
+        poweredID: 'bzn--V8HvCSHSx2W4xcxiSrgFA',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: '', // leave as empty string if not needed
+      },
+    },
+  ],
+}
