b5500a58bd82677f3d368c2fb01962b35c9e61a7
stefantrinh1 <stefantrinh1.work@gmail.com>
2021-05-28 12:51:45 -0600

    [skip ci] adding ne temp site la pincate bistro

---
+++ laPicanteBistro/gatsby-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-eLbyXbTETXiXSwyiG8Bjmg',
+        poweredID: 'bzn-eLbyXbTETXiXSwyiG8Bjmg',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: '', // leave as empty string if not needed
+      },
+    },
+  ],
+}
