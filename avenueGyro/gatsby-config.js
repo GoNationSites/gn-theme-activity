2a84cd209eb7dfcb3591f8b663d04b97c64c6010
stefantrinh1 <stefantrinh1.work@gmail.com>
2021-06-03 11:33:41 -0600

    [skip ci] adding new temp site avenue gyro

---
+++ avenueGyro/gatsby-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-Bxd0gqNbT8W4Bz676Rf_aA',
+        poweredID: 'bzn-Bxd0gqNbT8W4Bz676Rf_aA',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: '', // leave as empty string if not needed
+      },
+    },
+  ],
+}
