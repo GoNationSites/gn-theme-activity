be73a875d8e44df5bd904c0d503b9c1e48e3f006
stefantrinh1 <stefantrinh1.work@gmail.com>
2021-06-11 08:30:29 -0600

    [skip ci] adding new site organika cafe

---
+++ organikaCafe/gatsby-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-2IRal-HaTwiRtQLMH6P20g',
+        poweredID: 'bzn-2IRal-HaTwiRtQLMH6P20g',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: '', // leave as empty string if not needed
+      },
+    },
+  ],
+}
