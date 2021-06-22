c9c70ce898bdcd31ee24aa7e2f186f9d1ae49048
stefantrinh1 <stefantrinh1.work@gmail.com>
2021-06-22 15:26:36 -0600

    [skip ci] adding no default image for shout

--- gatsby-theme-minimal/src/components/Shout.js
+++ gatsby-theme-minimal/src/components/Shout.js
@@ -1,116 +1,112 @@
-import React, { useEffect, useState, useRef } from 'react';
-import { Box, Image, Flex, Text, Link } from 'theme-ui';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faComment } from '@fortawesome/free-solid-svg-icons';
-import checkIfExternalLink from '../helpers/checkIfExternalLink';
-import Lightbox from 'react-image-lightbox';
-import 'react-image-lightbox/style.css';
-let axios = require('axios');
-let jsonpAdapter = require('axios-jsonp');
+import React, { useEffect, useState, useRef } from 'react'
+import { Box, Image, Flex, Text, Link } from 'theme-ui'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faComment } from '@fortawesome/free-solid-svg-icons'
+import checkIfExternalLink from '../helpers/checkIfExternalLink'
+import Lightbox from 'react-image-lightbox'
+import 'react-image-lightbox/style.css'
+let axios = require('axios')
+let jsonpAdapter = require('axios-jsonp')
 
 const Shout = ({ gonationID, poweredID }) => {
   const [shout, setShout] = useState({
     shoutData: null,
     isLoading: true,
-  });
-  const [scrolled, SetScrolled] = useState(false);
+  })
+  const [scrolled, SetScrolled] = useState(false)
 
   //  === State ===
   const [lightBox, setLightbox] = useState({
     isOpen: false,
     mainSrc: '',
-  });
+  })
 
-  const scrollRef = useRef();
-  scrollRef.current = scrolled;
+  const scrollRef = useRef()
+  scrollRef.current = scrolled
 
   useEffect(() => {
     const handleScroll = () => {
-      const show = window.scrollY > 70;
+      const show = window.scrollY > 70
       if (scrollRef.current !== show) {
-        SetScrolled(show);
+        SetScrolled(show)
       }
-    };
+    }
 
-    document.addEventListener('scroll', handleScroll);
-  }, []);
+    document.addEventListener('scroll', handleScroll)
+  }, [])
 
   useEffect(() => {
     axios({
       url: `https://data.prod.gonation.com/profile/shoutsnew/${poweredID}`,
       adapter: jsonpAdapter,
     })
-      .then((res) => {
-        setShout({ ...shout, shoutData: res.data, isLoading: false });
+      .then(res => {
+        setShout({ ...shout, shoutData: res.data, isLoading: false })
       })
-      .catch((e) => {
-        console.log('error : ', console.e);
-        setShout({ ...shout, isLoading: false });
-      });
-  }, []);
+      .catch(e => {
+        console.log('error : ', console.e)
+        setShout({ ...shout, isLoading: false })
+      })
+  }, [])
 
   return (
     <>
       {!shout.isLoading && shout.shoutData ? (
         <Box variant={`shout.${!scrolled ? 'container' : 'containerScrolled'}`}>
           <Flex variant='shout.innerContainer' className='shoutInnerContainer'>
-            <Box variant='shout.imageContainer' className='shoutImageContainer'>
-              <Image
-                src={`${shout.shoutData.imageBaseUrl}/${shout.shoutData.shout.image.image.cloudinaryId}`}
-                variant='shout.shoutImage'
-                className='shoutShoutImage'
-                alt='branding'
-                onClick={
-                  shout?.shoutData?.shout?.image?.image?.cloudinaryId
-                    ? () => {
-                        setLightbox({
-                          isOpen: true,
-                          mainSrc: `${shout.shoutData.imageBaseUrl}/${shout.shoutData.shout.image.image.cloudinaryId}`,
-                        });
-                      }
-                    : () => {}
-                }
-              />
-            </Box>
+            {console.log(shout.shoutData.shout.image)}
+            {!shout.shoutData.shout.image.isDefault ? (
+              <Box variant='shout.imageContainer' className='shoutImageContainer'>
+                <Image
+                  src={`${shout.shoutData.imageBaseUrl}/${shout.shoutData.shout.image.image.cloudinaryId}`}
+                  variant='shout.shoutImage'
+                  className='shoutShoutImage'
+                  alt='branding'
+                  onClick={
+                    shout?.shoutData?.shout?.image?.image?.cloudinaryId
+                      ? () => {
+                          setLightbox({
+                            isOpen: true,
+                            mainSrc: `${shout.shoutData.imageBaseUrl}/${shout.shoutData.shout.image.image.cloudinaryId}`,
+                          })
+                        }
+                      : () => {}
+                  }
+                />
+              </Box>
+            ) : (
+              ''
+            )}
 
             <Box variant='shout.content' className='shoutContent'>
               <Text variant='shout.title' className='shoutTitle'>
                 <FontAwesomeIcon icon={faComment} /> Recent Shout
               </Text>
-              <Text
-                variant='shout.text'
-                sx={{ fontSize: 2, margin: 0 }}
-                className='shoutText'
-              >
+              <Text variant='shout.text' sx={{ fontSize: 2, margin: 0 }} className='shoutText'>
                 {shout?.shoutData?.shout?.text}
               </Text>
 
               {shout?.shoutData?.shout?.ctas &&
-                Object.keys(shout?.shoutData?.shout?.ctas).map(
-                  (ctaName, index) => {
-                    if (ctaName === 'cta1' || ctaName === 'cta2') {
-                      return '';
-                    } else {
-                      return (
-                        <Link
-                          key={ctaName + index}
-                          variant={`shout.cta`}
-                          className='shoutCTA'
-                          href={shout?.shoutData?.shout?.ctas[ctaName]}
-                          target={
-                            checkIfExternalLink(
-                              shout?.shoutData?.shout?.ctas[ctaName]
-                            )
-                              ? '_blank'
-                              : ''
-                          }
-                        >
-                          {ctaName}
-                        </Link>
-                      );
-                    }
+                Object.keys(shout?.shoutData?.shout?.ctas).map((ctaName, index) => {
+                  if (ctaName === 'cta1' || ctaName === 'cta2') {
+                    return ''
+                  } else {
+                    return (
+                      <Link
+                        key={ctaName + index}
+                        variant={`shout.cta`}
+                        className='shoutCTA'
+                        href={shout?.shoutData?.shout?.ctas[ctaName]}
+                        target={
+                          checkIfExternalLink(shout?.shoutData?.shout?.ctas[ctaName])
+                            ? '_blank'
+                            : ''
+                        }>
+                        {ctaName}
+                      </Link>
+                    )
                   }
-                )}
+                })}
             </Box>
           </Flex>
         </Box>
@@ -125,7 +121,7 @@ const Shout = ({ gonationID, poweredID }) => {
         />
       )}
     </>
-  );
-};
+  )
+}
 
-export default Shout;
+export default Shout
