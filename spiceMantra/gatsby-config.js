2deaee0255fe60bd083bb6acdb94d49a5987d247
esmith7196 <ericsmith.100@gmail.com>
2021-07-01 13:17:05 -0400

    [skip ci] Add 2 new temp sites

---
+++ spiceMantra/gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-Oi1yn1MqSkGeGRzL9rHzHg',
+        poweredID: 'bzn-Oi1yn1MqSkGeGRzL9rHzHg',
+        seoKeywords: 'Spice Mantra',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink:
+          'https://slicelife.com/restaurants/ct/stamford/06902/t-s-pizza-kitchen/menu?utm_campaign=direct_websites', // leave as empty string if not needed
+      },
+    },
+  ],
+};
---
+++ tsPizzaKitchen/gatsby-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-sUz7YFcmR_K788akY-LKCQ',
+        poweredID: 'bzn-sUz7YFcmR_K788akY-LKCQ',
+        seoKeywords: "T's Pizza Kitchen",
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: '', // leave as empty string if not needed
+      },
+    },
+  ],
+};
