e1de3c2de556cb48d12c01c91e971ee80911cb2f
stefantrinh1 <stefantrinh1.work@gmail.com>
2021-07-06 11:23:13 -0600

    [skip ci] adding new cast iron temp page

---
+++ castIronKitchen/gatsby-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-qcFtFY_uRIywwIXA2sEGdQ',
+        poweredID: 'bzn-qcFtFY_uRIywwIXA2sEGdQ',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: '', // leave as empty string if not needed
+      },
+    },
+  ],
+}
