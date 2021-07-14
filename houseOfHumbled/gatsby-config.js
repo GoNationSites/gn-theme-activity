3294ad76a943ee9df9f2aad5c60f1c43b3162d64
stefantrinh1 <stefantrinh1.work@gmail.com>
2021-07-14 14:52:13 -0600

    [skip ci] adding new temp page house of humbled

---
+++ houseOfHumbled/gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-nv36a6rkSxWDBQG9s353aQ',
+        poweredID: 'bzn-nv36a6rkSxWDBQG9s353aQ',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink:
+          '', // leave as empty string if not needed
+      },
+    },
+  ],
+};
