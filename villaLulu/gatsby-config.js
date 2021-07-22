3d5f670612351ef61342b4cb656d332454f8e582
stefantrinh1 <stefantrinh1.work@gmail.com>
2021-07-22 10:05:42 -0600

    [skip ci] new temp page

---
+++ villaLulu/gatsby-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-TqdYTHIQTfeP9h5YBLFYCA',
+        poweredID: 'bzn-TqdYTHIQTfeP9h5YBLFYCA',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: '', // leave as empty string if not needed
+      },
+    },
+  ],
+}
