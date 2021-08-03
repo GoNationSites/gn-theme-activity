57929d92e116950f5978d4d08f815262ab95ceaf
stefantrinh1 <stefantrinh1.work@gmail.com>
2021-08-03 08:56:54 -0600

    [skip ci] adding new soho bar temp site

---
+++ sohoBar/gatsby-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-5Vy80boQSyuFYp3SSaKmsA',
+        poweredID: 'bzn-5Vy80boQSyuFYp3SSaKmsA',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: false,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: '', // leave as empty string if not needed
+      },
+    },
+  ],
+}
