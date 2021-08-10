09f30efd10f4f32091ac3fe42b15e0b3a6a824a6
stefantrinh1 <stefantrinh1.work@gmail.com>
2021-08-10 07:39:25 -0600

    [skip ci] adding new temp page taco house

---
+++ tacoHouse/gatsby-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-Xov3V64mQ2izEC6qEFEmgw',
+        poweredID: 'bzn-Xov3V64mQ2izEC6qEFEmgw',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: '', // leave as empty string if not needed
+      },
+    },
+  ],
+}
