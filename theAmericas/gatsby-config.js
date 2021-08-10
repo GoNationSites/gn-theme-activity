bfb856959b4c916f58e614be31fa45a828b297d9
stefantrinh1 <stefantrinh1.work@gmail.com>
2021-08-10 07:50:06 -0600

    [skip ci] adding americas temp page

---
+++ theAmericas/gatsby-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-bMrBhzM1SdG40ty2yFGQ2w',
+        poweredID: 'bzn-bMrBhzM1SdG40ty2yFGQ2w',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: '', // leave as empty string if not needed
+      },
+    },
+  ],
+}
