9e4f9400dd0823efd4f7d774ec5de75f404e5b3b
stefantrinh1 <stefantrinh1.work@gmail.com>
2021-08-18 08:38:46 -0600

    [skip ci] adding new limani site

---
+++ limaniGrille/gatsby-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-1hzNv2u7SEO03_0-uRvKLw',
+        poweredID: 'bzn-1hzNv2u7SEO03_0-uRvKLw',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: '', // leave as empty string if not needed
+      },
+    },
+  ],
+}
