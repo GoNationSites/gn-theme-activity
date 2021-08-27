b787687d24a304430bcf373b54269898f1b14a14
stefantrinh1 <stefantrinh1.work@gmail.com>
2021-08-27 08:15:33 -0600

    [skip ci] adding changes to the dining room

---
+++ theDiningRoom/gatsby-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-MOBqFgehTfyBys_q9enAtA',
+        poweredID: 'bzn-MOBqFgehTfyBys_q9enAtA',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: '', // leave as empty string if not needed
+      },
+    },
+  ],
+}
