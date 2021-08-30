9fdefe7cbd8b4bce4e86ef26c95a96fca2af31bd
stefantrinh1 <stefantrinh1.work@gmail.com>
2021-08-30 09:09:14 -0600

    [skip ci] adding new sitre via gastropub

---
+++ viaGastrobar/gatsby-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-8i-6plutSdm4lJbEQ2CWlg',
+        poweredID: 'bzn-8i-6plutSdm4lJbEQ2CWlg',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: '', // leave as empty string if not needed
+      },
+    },
+  ],
+}
