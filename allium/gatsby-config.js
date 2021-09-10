f34bf4cbdd82c5f7546f5573949d1aba54661b4f
stefantrinh1 <stefantrinh1.work@gmail.com>
2021-09-10 09:58:52 -0600

    [skip ci] adding new allium temp page

---
+++ allium/gatsby-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-AI__qZTaTo6blNq8AxSb9Q',
+        poweredID: 'bzn-AI__qZTaTo6blNq8AxSb9Q',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: '', // leave as empty string if not needed
+      },
+    },
+  ],
+}
