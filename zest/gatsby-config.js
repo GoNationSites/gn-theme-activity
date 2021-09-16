e41d1163d8bb64559752a099757383db7e145931
stefantrinh1 <stefantrinh1.work@gmail.com>
2021-09-16 09:59:36 -0600

    [skip ci] adding new zest site

---
+++ zest/gatsby-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-mKDl97AuSMW4sCH9tOoOzw',
+        poweredID: 'bzn-mKDl97AuSMW4sCH9tOoOzw',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: '', // leave as empty string if not needed
+      },
+    },
+  ],
+}
