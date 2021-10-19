800b15981a5708654ae57dae317a574912e31dbf
stefantrinh1 <stefantrinh1.work@gmail.com>
2021-10-19 12:46:44 -0600

    [skip ci] adding new site tasty yolk stratford

---
+++ tastyYolkStratford/gatsby-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-xGPaoSMrQveu0z-GIYwa7g',
+        poweredID: 'bzn-xGPaoSMrQveu0z-GIYwa7g',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: '', // leave as empty string if not needed
+      },
+    },
+  ],
+}
