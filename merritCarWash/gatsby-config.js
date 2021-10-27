24c64f73459471634689674b82e224f7a701d4b0
stefantrinh1 <stefantrinh1.work@gmail.com>
2021-10-27 09:05:24 -0600

    [skip ci] adding new temp site

---
+++ merritCarWash/gatsby-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-xjuUPLFEQVGzY1w0wTke8w',
+        poweredID: 'bzn-xjuUPLFEQVGzY1w0wTke8w',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: '', // leave as empty string if not needed
+      },
+    },
+  ],
+}
