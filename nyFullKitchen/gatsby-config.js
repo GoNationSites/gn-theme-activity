5ac2196f56b6a8607bda288ce32205d9e2dbd855
stefantrinh1 <stefantrinh1.work@gmail.com>
2021-11-01 12:39:50 -0600

    [skip ci] adding new temp site ny full kitchen

---
+++ nyFullKitchen/gatsby-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-i26mzDlTQvuroyox6lyuUw',
+        poweredID: 'bzn-i26mzDlTQvuroyox6lyuUw',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: 'https://orderonlinemenu.com/NYFullKitchen', // leave as empty string if not needed
+      },
+    },
+  ],
+}
