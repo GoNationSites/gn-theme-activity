0a9a844186d9322008957aa0d03bb867124ec0fe
stefantrinh1 <stefantrinh1.work@gmail.com>
2021-11-10 15:03:41 -0700

    [skip ci] adding new site saltwater to temp pages

---
+++ saltwaterRestaurant/gatsby-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-hLJXrm-4QKmNeZf4oEi3bw',
+        poweredID: 'bzn-hLJXrm-4QKmNeZf4oEi3bw',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: '', // leave as empty string if not needed
+      },
+    },
+  ],
+}
