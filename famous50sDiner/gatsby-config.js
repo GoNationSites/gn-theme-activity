3b1d8c334187a23482e4c2fbe028e9fa01f809ca
stefantrinh1 <stefantrinh1.work@gmail.com>
2021-11-11 11:57:38 -0700

    [skip ci] adding famous 50s dinner

---
+++ famous50sDiner/gatsby-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-pxZXxTUwSL2xOjcujQd8Pg',
+        poweredID: 'bzn-pxZXxTUwSL2xOjcujQd8Pg',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: '', // leave as empty string if not needed
+      },
+    },
+  ],
+}
