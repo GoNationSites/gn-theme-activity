243c72a7d83a238a8181a54af00ddf3d3e573ce0
stefantrinh1 <stefantrinh1.work@gmail.com>
2021-12-15 15:31:20 -0700

    [skip ci] adding new site

---
+++ magicPiePizza/gatsby-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-b0J7MXlYRL_bjcwo-ZG3_g',
+        poweredID: 'bzn-b0J7MXlYRL_bjcwo-ZG3_g',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: '', // leave as empty string if not needed
+      },
+    },
+  ],
+}
