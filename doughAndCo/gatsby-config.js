fd1cae2b20cea193d336624f0bfd4e096b9b8109
stefantrinh1 <stefantrinh1.work@gmail.com>
2022-02-01 07:59:23 -0700

    [skip ci] new site

---
+++ doughAndCo/gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-OW4d9mGcRRy1A0DgD9CmSQ',
+        poweredID: 'bzn-OW4d9mGcRRy1A0DgD9CmSQ',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: '', // leave as empty string if not needed
+        orderOnlineName: '',
+      },
+    },
+  ],
+}
