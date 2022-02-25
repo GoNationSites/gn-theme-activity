48244be6e03dce67b6a13fdefb07a552cc6ef5a3
stefantrinh1 <stefantrinh1.work@gmail.com>
2022-02-25 08:26:15 -0700

    [skip ci] adding new site sweet leaf

---
+++ sweetLeafLiving/gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-L7N5wtHrQ5y9OgsV8REsYw',
+        poweredID: 'bzn-L7N5wtHrQ5y9OgsV8REsYw',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: '', // leave as empty string if not needed
+        orderOnlineName: '',
+      },
+    },
+  ],
+};
