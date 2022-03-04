6cdb4f1741f5717ed4c599baf640d2cf07c152f5
stefantrinh1 <stefantrinh1.work@gmail.com>
2022-03-04 14:01:54 -0700

    [skip ci] adding new page

---
+++ chacra/gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn--xgYIDyGR82yHrw7aU0yNA',
+        poweredID: 'bzn--xgYIDyGR82yHrw7aU0yNA',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: '', // leave as empty string if not needed
+        orderOnlineName: '',
+      },
+    },
+  ],
+};
---
+++ chacra/src/gatsby-theme-minimal/gatsby-plugin-theme-ui/colors.js
@@ -0,0 +1,7 @@
+export default {
+  text: '#2c2222',
+  background: '#fefefe',
+  primary: '#202020',
+  secondary: '#202020',
+  light: '#FFF',
+}
