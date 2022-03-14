2cdc2385b2acc6ca94465fa3ad5987b83efe12e3
stefantrinh1 <stefantrinh1.work@gmail.com>
2022-03-14 14:12:03 -0600

    [skip ci] adding new ativity site mamma bella

---
+++ mammaBella/gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-m9JszuE3S56f1VbubSFfBA',
+        poweredID: 'bzn-m9JszuE3S56f1VbubSFfBA',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: '', // leave as empty string if not needed
+        orderOnlineName: '',
+      },
+    },
+  ],
+};
