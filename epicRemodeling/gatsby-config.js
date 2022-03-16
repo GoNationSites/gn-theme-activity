40857eda0fe44098f975e486c66c5ca919dd69fd
esmith7196 <ericsmith.100@gmail.com>
2022-03-16 17:46:17 -0400

    [skip ci] Add epic remodeling

---
+++ epicRemodeling/gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-Jp7FJLuaSRqrOq5f_TZUKg',
+        poweredID: 'bzn-Jp7FJLuaSRqrOq5f_TZUKg',
+        seoKeywords: 'Epic remodeling construction',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: '', // leave as empty string if not needed
+        orderOnlineName: '',
+      },
+    },
+  ],
+};
