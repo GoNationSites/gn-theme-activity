648e96b38a8ca2dc89586844b808ab2c9e4c4355
stefantrinh1 <stefantrinh1.work@gmail.com>
2022-03-25 14:11:23 -0600

    [skip ci] adding changes to prime time

---
+++ primeTimeCatering/gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-WcB5jEvvTWGemWoYNPJ1vg',
+        poweredID: 'bzn-WcB5jEvvTWGemWoYNPJ1vg',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: '', // leave as empty string if not needed
+        orderOnlineName: '',
+      },
+    },
+  ],
+};
