9bde358972a8294a853b09f36d546d70bff93a0e
esmith7196 <ericsmith.100@gmail.com>
2022-05-04 15:51:10 -0400

    [skip ci] Add southport fairfield

---
+++ southbay/gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-wDo_NujiRpSOw4MkHilXbQ',
+        poweredID: 'bzn-wDo_NujiRpSOw4MkHilXbQ',
+        seoKeywords: 'Southbay Fairfield',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: '', // leave as empty string if not needed
+        orderOnlineName: '',
+      },
+    },
+  ],
+};
