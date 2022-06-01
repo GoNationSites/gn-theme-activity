80ccdb4b4b1ef1f115cfb276f404201be93f0884
esmith7196 <ericsmith.100@gmail.com>
2022-06-01 12:03:24 -0400

    [skip ci] Add ilPellicano 

---
+++ ilPellicano /gatsby-config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-mrOo7JEsT6KqhYg_N5vOJg',
+        poweredID: 'bzn-mrOo7JEsT6KqhYg_N5vOJg',
+        seoKeywords: ` italian restaurant in fairfield, ct
+best wine bar in fairfield, ct
+best craft cocktails in fairfield, ct
+outdoor dining in fairfield, ct
+best rooftop in ct
+kid friendly restaurant in fairfield, ct
+best takeout in fairfield, ct
+best tapas in fairfield, ct`,
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: '', // leave as empty string if not needed
+        orderOnlineName: '',
+      },
+    },
+  ],
+};
