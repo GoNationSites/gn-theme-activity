db688adbf2c4fcd9a850d019efd632557418244a
esmith7196 <ericsmith.100@gmail.com>
2022-06-10 14:19:38 -0400

    [skip ci] add baldo bistro and make logo of prime black bg

---
+++ baldoBistro/gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-1AYLOYMBRlKe8UQPcL-0XQ',
+        poweredID: 'bzn-1AYLOYMBRlKe8UQPcL-0XQ',
+        seoKeywords: 'Baldo Bistro new jersey lakeside road',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: '', // leave as empty string if not needed
+        orderOnlineName: '',
+      },
+    },
+  ],
+};
