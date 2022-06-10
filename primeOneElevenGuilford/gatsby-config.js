ea25ad1cf6584896d9e0767712de35e93cef8d30
esmith7196 <ericsmith.100@gmail.com>
2022-06-10 14:05:52 -0400

    [skip ci] Add prime one eleven guilford

---
+++ primeOneElevenGuilford/gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-aH6mdl8ZS4_kg_56QsVeWA',
+        poweredID: 'bzn-aH6mdl8ZS4_kg_56QsVeWA',
+        seoKeywords: 'Prime One Eleven Guilford',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: '', // leave as empty string if not needed
+        orderOnlineName: '',
+      },
+    },
+  ],
+};
