151e056362fd10e48007ad14f0f3d7bd4c8d7609
esmith7196 <ericsmith.100@gmail.com>
2022-06-24 16:18:31 -0400

    [skip ci] Add new activity page

---
+++ roseapple/gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-lTBTuIcLRYS7T-_zZTt-oQ',
+        poweredID: 'bzn-lTBTuIcLRYS7T-_zZTt-oQ',
+        seoKeywords: 'Flames Restaurant',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: '', // leave as empty string if not needed
+        orderOnlineName: '',
+      },
+    },
+  ],
+};
