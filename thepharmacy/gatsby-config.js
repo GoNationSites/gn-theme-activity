6bf2ff97ed2f2568c5d46ad8a3683cc82b1e9791
esmith7196 <ericsmith.100@gmail.com>
2022-06-28 15:13:26 -0400

    [skip ci] Add new activity

---
+++ thepharmacy/gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-UsxFqO41RZeiT_GfXp8H2g',
+        poweredID: 'bzn-UsxFqO41RZeiT_GfXp8H2g',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: '', // leave as empty string if not needed
+        orderOnlineName: '',
+      },
+    },
+  ],
+};
