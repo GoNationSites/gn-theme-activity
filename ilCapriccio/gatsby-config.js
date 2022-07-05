bc8c93152abd53a89c2665f58404bba40f2dd4f4
esmith7196 <ericsmith.100@gmail.com>
2022-07-05 09:45:52 -0400

    [skip ci] Add ilCapriccio

---
+++ ilCapriccio/gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-x6ALipnWTq6C0gBZ_tybqg',
+        poweredID: 'bzn-x6ALipnWTq6C0gBZ_tybqg',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: '', // leave as empty string if not needed
+        orderOnlineName: '',
+      },
+    },
+  ],
+};
---
+++ ilCapriccio/src/gatsby-theme-minimal/gatsby-plugin-theme-ui/colors.js
@@ -0,0 +1,7 @@
+export default {
+  text: '#2c2222',
+  background: '#fefefe',
+  primary: '#008A48',
+  secondary: '#008A48',
+  light: '#FFF',
+};
