2584dda46e4c5e988ad9770a1353b59a1a806dd3
esmith7196 <ericsmith.100@gmail.com>
2022-07-25 11:11:38 -0400

    [skip ci] Add 2 new activities

---
+++ flcInsurance/gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-i8V0jhRITGug2yepZE2Vvg',
+        poweredID: 'bzn-i8V0jhRITGug2yepZE2Vvg',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: '', // leave as empty string if not needed
+        orderOnlineName: '',
+      },
+    },
+  ],
+};
