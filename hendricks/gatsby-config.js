9fc5c9bc4e42a6cd160f6fcd9b1662f1386bb5f7
esmith7196 <ericsmith.100@gmail.com>
2022-07-25 11:22:27 -0400

    [skip ci] Add hendricks

---
+++ hendricks/gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-w3L0St1tTMuotvRzy1OFdA',
+        poweredID: 'bzn-w3L0St1tTMuotvRzy1OFdA',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: '', // leave as empty string if not needed
+        orderOnlineName: '',
+      },
+    },
+  ],
+};
