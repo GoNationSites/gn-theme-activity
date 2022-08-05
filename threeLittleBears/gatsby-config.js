31756bb076fce02a81c75cfd4a899af85ded31ff
esmith7196 <ericsmith.100@gmail.com>
2022-08-05 16:36:27 -0400

    [skip ci] Add 3 little bears and gregorys

---
+++ threeLittleBears/gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-eRFLp9kyQxSWcukfCso_KA',
+        poweredID: 'bzn-eRFLp9kyQxSWcukfCso_KA',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: '', // leave as empty string if not needed
+        orderOnlineName: '',
+      },
+    },
+  ],
+};
