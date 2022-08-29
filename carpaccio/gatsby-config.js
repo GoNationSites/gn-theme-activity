252017bfd66802b579df3cb6f58edff317c26a46
esmith7196 <ericsmith.100@gmail.com>
2022-08-29 18:18:03 -0400

    [skip ci] Add carpaccio

---
+++ carpaccio/gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-PkKIGJCrTt2Qv8n4ucw18w',
+        poweredID: 'bzn-PkKIGJCrTt2Qv8n4ucw18w',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: '', // leave as empty string if not needed
+        orderOnlineName: '',
+      },
+    },
+  ],
+};
