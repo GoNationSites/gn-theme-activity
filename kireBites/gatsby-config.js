b43d9e826f34778c09e5538883cbd18dadb5e7fa
Gabriel Pereira <omami.thatsgood@gmail.com>
2022-09-28 15:04:15 -0400

    [skip ci] kire bites activity page

---
+++ kireBites/gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-xcjaQE8lT9_poJ5xwDpJvg',
+        poweredID: 'bzn-xcjaQE8lT9_poJ5xwDpJvg',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: '', // leave as empty string if not needed
+        orderOnlineName: '',
+      },
+    },
+  ],
+};
