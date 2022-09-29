134d91f030fb85c072f0df7795d3ae627583a836
Gabriel Pereira <omami.thatsgood@gmail.com>
2022-09-29 13:02:19 -0400

    [skip ci] bagel boutique activity page

---
+++ bagelBoutique/gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-xMQdH0KzRBOzr_HkdktVXQ',
+        poweredID: 'bzn-xMQdH0KzRBOzr_HkdktVXQ',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: '', // leave as empty string if not needed
+        orderOnlineName: '',
+      },
+    },
+  ],
+};
