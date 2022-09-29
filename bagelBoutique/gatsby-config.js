e1f02c10be5bc4d87fc50975eaa70a5237aeb5b8
Gabriel Pereira <omami.thatsgood@gmail.com>
2022-09-29 13:16:15 -0400

    [skip ci]

--- bagelBoutique/gatsby-config.js
+++ bagelBoutique/gatsby-config.js
@@ -11,6 +11,7 @@ module.exports = {
         hasShout: true,
         hasHours: true,
         hasContact: true,
+      
         orderOnlineLink: '', // leave as empty string if not needed
         orderOnlineName: '',
       },
