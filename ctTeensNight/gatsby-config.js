5127d11f4de3570eb716823ae788208cd4ecb6c2
Gabriel Pereira <omami.thatsgood@gmail.com>
2022-10-06 12:05:57 -0400

    [skip ci] ct Teen night

---
+++ ctTeensNight/gatsby-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-YEArjvYhTtSEfxZQaDbkrw',
+        poweredID: 'bzn-YEArjvYhTtSEfxZQaDbkrw',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+      
+        orderOnlineLink: '', // leave as empty string if not needed
+        orderOnlineName: '',
+      },
+    },
+  ],
+};
