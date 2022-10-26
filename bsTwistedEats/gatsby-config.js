8832ad9625fd2485bfc717e86824238ca3bcf233
Gabriel Pereira <omami.thatsgood@gmail.com>
2022-10-26 12:24:00 -0400

    [skip ci] initial theme page

---
+++ bsTwistedEats/gatsby-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn--oqGWF0IR0mdPFBtSdS9pA',
+        poweredID: 'bzn--oqGWF0IR0mdPFBtSdS9pA',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+      
+        orderOnlineLink: '', // leave as empty string if not needed
+        orderOnlineName: '',
+      },
+    },
+  ],
+};
