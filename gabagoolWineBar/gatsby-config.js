a0b241f78ae77bc9f0a756a2172607b897447c0c
Gabriel Pereira <omami.thatsgood@gmail.com>
2022-10-28 11:44:10 -0400

    [skip ci] adding gabagool wine bar

---
+++ gabagoolWineBar/gatsby-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-lOV1wvl1QIGfQZ4-jixMYA',
+        poweredID: 'bzn-lOV1wvl1QIGfQZ4-jixMYA',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+      
+        orderOnlineLink: '', // leave as empty string if not needed
+        orderOnlineName: '',
+      },
+    },
+  ],
+};
