2e42b05d3a2fff749c4981388b2a92cc62674c68
Gabriel Pereira <omami.thatsgood@gmail.com>
2022-11-08 13:25:52 -0500

    [skip ci] adding luna

---
+++ luna/gatsby-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-11_RofNnR6W-IG1fvhxsbg',
+        poweredID: 'bzn-11_RofNnR6W-IG1fvhxsbg', 
+        seoKeywords: ' ',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+      
+        orderOnlineLink: '', // leave as empty string if not needed
+        orderOnlineName: '',
+      },
+    },
+  ],
+};
