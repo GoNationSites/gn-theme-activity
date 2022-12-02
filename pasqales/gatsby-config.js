7c5f6acdbfca70f3e0c3d888d4bd18aa194af9bd
Gabriel Pereira <omami.thatsgood@gmail.com>
2022-12-02 12:25:33 -0500

    [skip ci] adding pasqales

---
+++ pasqales/gatsby-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-siiXJ3erQyOGKjWSWPNNgA',
+        poweredID: 'bzn-siiXJ3erQyOGKjWSWPNNgA', 
+        seoKeywords: ' ',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+      
+        orderOnlineLink: '', // leave as empty string if not needed
+        orderOnlineName: '',
+      },
+    },
+  ],
+};
