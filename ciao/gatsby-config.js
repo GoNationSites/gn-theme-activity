c0f4d33fa57a5426092b8f17dbd01719dce46d6f
Gabriel Pereira <omami.thatsgood@gmail.com>
2022-12-07 13:53:12 -0500

    [skip ci] adding ciao

---
+++ ciao/gatsby-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-c0T9_eohRDO97U6rsso7HA',
+        poweredID: 'bzn-c0T9_eohRDO97U6rsso7HA', 
+        seoKeywords: ' ',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+      
+        orderOnlineLink: '', // leave as empty string if not needed
+        orderOnlineName: '',
+      },
+    },
+  ],
+};
