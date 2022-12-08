3e676a5b5cc1c37788db5dc3c95fa4bf045e3953
Gabriel Pereira <omami.thatsgood@gmail.com>
2022-12-08 09:47:14 -0500

    [skip ci] adding theRidges

---
+++ theRidges/gatsby-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-IKqXywHAT7acaUObaas2aA',
+        poweredID: 'bzn-IKqXywHAT7acaUObaas2aA', 
+        seoKeywords: ' ',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+      
+        orderOnlineLink: 'https://www.opentable.com/r/the-ridges-branford?ref=1068', // leave as empty string if not needed
+        orderOnlineName: 'Reservation',
+      },
+    },
+  ],
+};
