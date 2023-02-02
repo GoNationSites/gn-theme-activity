4c37398bfafa21f12d2f8a204262f1bd4e08b1b6
Gabriel Pereira <omami.thatsgood@gmail.com>
2023-02-02 15:53:23 -0500

    [skip ci] adding sugo

---
+++ sugo-gastro/gatsby-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn--sFVtazzShiPv6dCTx-Ybg',
+        poweredID: 'bzn--sFVtazzShiPv6dCTx-Ybg',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+      
+        orderOnlineLink: '', // leave as empty string if not needed
+        orderOnlineName: '',
+      },
+    },
+  ],
+};
