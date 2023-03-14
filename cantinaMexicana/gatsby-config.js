cf7674131b70bce13986ec4c60cac7be66f8f04b
Gabriel Pereira <omami.thatsgood@gmail.com>
2023-03-14 16:53:07 -0400

    [skip ci] adding cantina

---
+++ cantinaMexicana/gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-vvZe_vb2St27Rpwbrp2hlA',
+        poweredID: '570e75a07455de312b1d0fc9',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: '', // leave as empty string if not needed
+        orderOnlineName: '',
+      },
+    },
+  ],
+};
