e782c6c5b71f5bccd3083bca351bab9180864676
Gabriel Pereira <omami.thatsgood@gmail.com>
2023-03-20 16:55:48 -0400

    [skip ci] adding vespino

---
+++ vespinoPizza/gatsby-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-DUeS6oZ2R5moW313s8S1pQ',
+        poweredID: 'bzn-DUeS6oZ2R5moW313s8S1pQ', 
+        seoKeywords: ' ',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+      
+        orderOnlineLink: 'https://www.toasttab.com/vespino/v3', // leave as empty string if not needed
+        orderOnlineName: 'Order Online',
+      },
+    },
+  ],
+};
