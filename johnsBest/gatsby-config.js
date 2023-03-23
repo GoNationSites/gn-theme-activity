010e365c6c18da05b1b0e7f07be672af615d01cc
Gabriel Pereira <omami.thatsgood@gmail.com>
2023-03-23 15:27:18 -0400

    [skip ci] adding johnsBest

---
+++ johnsBest/gatsby-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-EYUT_dxVQVOhVYX42SNxcg',
+        poweredID: 'bzn-EYUT_dxVQVOhVYX42SNxcg', 
+        seoKeywords: ' ',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+      
+        orderOnlineLink: 'https://slicelife.com/restaurants/ct/norwalk/06851/john-s-best-jr-pizza/menu', // leave as empty string if not needed
+        orderOnlineName: 'Menu',
+      },
+    },
+  ],
+};
