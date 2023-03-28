a580f1bc721cc7d5eaa53f7ba6be4c7c6e4968c2
Gabriel Pereira <omami.thatsgood@gmail.com>
2023-03-28 09:40:37 -0400

    [skip ci] adding mil sabores

---
+++ milSabores/gatsby-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-Hr12FVAdSTCXmlq6egnkZQ',
+        poweredID: 'bzn-Hr12FVAdSTCXmlq6egnkZQ', 
+        seoKeywords: ' ',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+      
+        orderOnlineLink: 'Menu', // leave as empty string if not needed
+        orderOnlineName: 'https://res.cloudinary.com/gonation/image/upload/v1680010692/sites/milSabores/Mils_Sabores_Menu_V3-min.pdf',
+      },
+    },
+  ],
+};
