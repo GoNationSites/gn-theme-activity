5190e9f141b10b583512ae37fae59428ed152d88
Gabriel Pereira <omami.thatsgood@gmail.com>
2023-04-06 13:58:42 -0400

    [skip ci] adding jimmy

---
+++ jimmyMcGarth/gatsby-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-dRA6aXlGTbOsIa0FhNWcoA',
+        poweredID: 'bzn-dRA6aXlGTbOsIa0FhNWcoA', 
+        seoKeywords: ' ',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+      
+        orderOnlineLink: '', // leave as empty string if not needed
+        orderOnlineName: '',
+      },
+    },
+  ],
+};
