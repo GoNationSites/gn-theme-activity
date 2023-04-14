bd9285ff31192ac65fe43b174b5b097604dd53f2
Gabriel Pereira <omami.thatsgood@gmail.com>
2023-04-14 15:15:48 -0400

    [skip ci] adding tijuana

---
+++ tasteOfTijuana/gatsby-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-6UPnDepTSWSivM9oI8XxAA',
+        poweredID: 'bzn-6UPnDepTSWSivM9oI8XxAA', 
+        seoKeywords: ' ',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+      
+        orderOnlineLink: '', // leave as empty string if not needed
+        orderOnlineName: '',
+      },
+    },
+  ],
+};
