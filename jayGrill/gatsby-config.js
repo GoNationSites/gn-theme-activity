df0b26f329bb6cd3d53a05522310b947e14aefa1
Gabriel Pereira <omami.thatsgood@gmail.com>
2023-05-03 15:19:56 -0400

    [skip ci] adding jays

---
+++ jayGrill/gatsby-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-xPKnCXQHRNOOVp-MbYVFOg',
+        poweredID: 'bzn-xPKnCXQHRNOOVp-MbYVFOg', 
+        seoKeywords: ' ',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+      
+        orderOnlineLink: '', // leave as empty string if not needed
+        orderOnlineName: '',
+      },
+    },
+  ],
+};
