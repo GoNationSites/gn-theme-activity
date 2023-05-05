827e48f1e29cfd75eff3d55d84b2ec4f93833018
Gabriel Pereira <omami.thatsgood@gmail.com>
2023-05-05 10:24:27 -0400

    [skip ci] adding viaVeroli

---
+++ viaVeroli/gatsby-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-iPdEMVx9SDK8jN4-DPiauw',
+        poweredID: 'bzn-iPdEMVx9SDK8jN4-DPiauw', 
+        seoKeywords: ' ',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+      
+        orderOnlineLink: '', // leave as empty string if not needed
+        orderOnlineName: '',
+      },
+    },
+  ],
+};
