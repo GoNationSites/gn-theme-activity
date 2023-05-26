19afefd7611d0ddbdb73ed871702df8bf1d6e32d
Gabriel Pereira <omami.thatsgood@gmail.com>
2023-05-26 11:47:41 -0400

    [skip ci] adding landing at five twenty

---
+++ theLandingAtFiveTwenty/gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-MP8Pij34TkSiTxk_7heCjg',
+        poweredID: 'bzn-MP8Pij34TkSiTxk_7heCjg',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: 'https://res.cloudinary.com/gonation/image/upload/v1685115487/landingAtTwentyFive/Five-Twenty_3_1.pdf', // leave as empty string if not needed
+        orderOnlineName: 'Menu',
+      },
+    },
+  ],
+};
