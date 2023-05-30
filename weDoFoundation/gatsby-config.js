e84ec231b3b86e0831101948109f92c794d902b4
Gabriel Pereira <omami.thatsgood@gmail.com>
2023-05-30 10:48:17 -0400

    [skip ci] adding we do foundation

---
+++ weDoFoundation/gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: "bzn-35wWpQDSR8GTDsYQ59538g",
+        poweredID: "bzn-35wWpQDSR8GTDsYQ59538g",
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: 'https://res.cloudinary.com/gonation/image/upload/v1685115487/landingAtTwentyFive/Five-Twenty_3_1.pdf', // leave as empty string if not needed
+        orderOnlineName: 'Menu',
+      },
+    },
+  ],
+};
---
+++ weDoFoundation/src/components/events/Events.js
@@ -0,0 +1,137 @@
+/** @jsx jsx */
+import { jsx } from 'theme-ui';
+import React, { useEffect, useState } from 'react';
+import { Box, Flex, Text } from 'theme-ui';
+import EventItem from './EventItem';
+
+let axios = require('axios');
+let jsonpAdapter = require('axios-jsonp');
+
+const Events = ({ gonationID, slug, poweredID }) => {
+  const [recurringEvents, setRecurringEvents] = useState({
+    eventsData: null,
+    isLoading: true,
+    modalShowing: '',
+  });
+
+  const [singleEvents, setSingleEvents] = useState({
+    eventsData: null,
+    isLoading: true,
+    modalShowing: '',
+  });
+
+  useEffect(() => {
+    // fetch recurring events
+    axios({
+      url: `https://data.prod.gonation.com/profile/recurringevents?profile_id=${poweredID}`,
+      adapter: jsonpAdapter,
+    })
+      .then(res => {
+        setRecurringEvents({
+          ...recurringEvents,
+          eventsData: res.data,
+          isLoading: false,
+        });
+      })
+      .catch(e => {
+        console.log('error : ', console.e);
+        setRecurringEvents({ ...recurringEvents, isLoading: false });
+      });
+
+    // fetch special events
+    axios({
+      url: `https://data.prod.gonation.com/profile/events?profile_id=${poweredID}`,
+      adapter: jsonpAdapter,
+    })
+      .then(res => {
+        setSingleEvents({
+          ...singleEvents,
+          eventsData: res.data,
+          isLoading: false,
+        });
+      })
+      .catch(e => {
+        console.log('error : ', console.e);
+        setSingleEvents({ ...recurringEvents, isLoading: false });
+      });
+  }, []);
+
+  const closeModal = () => {
+    // maybe refactor at some point to recieve a type and then close the right one instead of close all
+    setSingleEvents({ ...singleEvents, modalShowing: '' });
+    setRecurringEvents({ ...recurringEvents, modalShowing: '' });
+  };
+
+  return (
+    <Box>
+      {!recurringEvents.isLoading &&
+      recurringEvents.eventsData &&
+      recurringEvents.eventsData.events.length ? (
+        // ========== Start RECURRINGS EVENTS JSX ==============
+        <Box variant="page.section">
+          <Text variant="sectionHeading">Recurring Events</Text>
+          <Flex variant="event.eventsContainer" sx={{ flexWrap: 'wrap' }}>
+            {recurringEvents.eventsData.events.map(event => {
+              return (
+                <Box
+                  sx={{
+                    width: ['100%', '50%', '33.333333%'],
+                  }}
+                >
+                  <EventItem
+                    key={event._id}
+                    event={event}
+                    setEvents={setRecurringEvents}
+                    closeModal={closeModal}
+                    state={recurringEvents}
+                    slug={slug}
+                  />
+                </Box>
+              );
+            })}
+          </Flex>
+        </Box>
+      ) : (
+        ''
+      )}
+
+      {!singleEvents.isLoading &&
+      singleEvents.eventsData &&
+      singleEvents.eventsData.events.length ? (
+        // ========== END OF RECURRINGS EVENTS JSX ============
+
+        // ======= Start of Single Events  =======
+        <Box variant="page.section">
+          <Text variant="sectionHeading">Special Events</Text>
+          <Flex variant="event.eventsContainer" sx={{ flexWrap: 'wrap' }}>
+            {singleEvents.eventsData.events.map(event => {
+              return (
+                <Box
+                  sx={{
+                    width: ['100%', '50%', '33.333333%'],
+                    padding: '0 10px',
+                  }}
+                >
+                  <EventItem
+                    key={event._id}
+                    event={event}
+                    setEvents={setRecurringEvents}
+                    closeModal={closeModal}
+                    state={recurringEvents}
+                    slug={slug}
+                  />
+                </Box>
+              );
+            })}
+          </Flex>
+        </Box>
+      ) : (
+        // ========== END OF RECURRINGS EVENTS JSX ============
+
+        ''
+      )}
+    </Box>
+  );
+};
+
+export default Events;
