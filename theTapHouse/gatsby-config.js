dc352f17b8b8b315cb64e7df78fd1ce0d6009aff
Stefan Trinh <32978225+stefantrinh1@users.noreply.github.com>
2023-07-14 06:53:17 -0700

    [skip ci] adding new temp page

---
+++ theTapHouse/gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-HJ-Q-CAKRxKMaY5_mGH-nQ',
+        poweredID: 'bzn-HJ-Q-CAKRxKMaY5_mGH-nQ',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineName: '',
+        orderOnlineLink: '', // leave as empty string if not needed
+      },
+    },
+  ],
+};
