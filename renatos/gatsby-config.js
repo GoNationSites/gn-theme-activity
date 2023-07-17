06928f9cd1a6abd3aa10e72e9e1ff2246d92f938
Stefan Trinh <32978225+stefantrinh1@users.noreply.github.com>
2023-07-17 09:09:41 -0700

    [skip ci] adding new site renatos

---
+++ renatos/gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-KjP_5jHXRx_wRcSzZ6TGpw',
+        poweredID: 'bzn-KjP_5jHXRx_wRcSzZ6TGpw',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineName: '',
+        orderOnlineLink: '', // leave as empty string if not needed
+      },
+    },
+  ],
+};
