868f8e5cd37c73ef32bb260627a11373fc46184a
esmith7196 <ericsmith.100@gmail.com>
2023-07-19 10:23:25 -0400

    [skip ci] Change merge strat

--- esh/src/gatsby-theme-minimal/components/cover/Cover.js
+++ esh/src/gatsby-theme-minimal/components/cover/Cover.js
@@ -1,28 +1,38 @@
 import React from 'react';
 import { Flex } from 'theme-ui';
 export default function Cover({ coverImageId }) {
-  return (
-    <Flex sx={styles.container}>
-      <video
-        autoPlay={true}
-        muted={true}
-        playsInline={true}
-        webkitPlaysInline={true}
-        controls={false}
-        loop={true}
-        poster={'https://res.cloudinary.com/gonation/' + coverImageId}
-        alt="coverposter"
-      >
-        <source
-          src={
-            'https://res.cloudinary.com/gonation/video/upload/q_auto/v1652234425/sites/esh/esh_banner_web_cut_out_10000.mp4'
-          }
-          type="video/mp4"
-        />
+  //   return (
+  //     <Flex sx={styles.container}>
+  //       <video
+  //         autoPlay={true}
+  //         muted={true}
+  //         playsInline={true}
+  //         webkitPlaysInline={true}
+  //         controls={false}
+  //         loop={true}
+  //         poster={'https://res.cloudinary.com/gonation/' + coverImageId}
+  //         alt="coverposter"
+  //       >
+  //         <source
+  //           src={
+  //             'https://res.cloudinary.com/gonation/video/upload/v1652234425/sites/esh/esh_banner_web_cut_out_10000.mp4'
+  //           }
+  //           type="video/mp4"
+  //         />
 
-        <track kind="captions" />
-      </video>
-    </Flex>
+  //         <track kind="captions" />
+  //       </video>
+  //     </Flex>
+  //   );
+  return (
+    <Flex
+      variant="cover"
+      title="main cover"
+      ariaLabel="main cover"
+      sx={{
+        backgroundImage: `linear-gradient(45deg, rgb(0 0 0 / 10%), rgb(0 0 0 / 10%)), url('https://res.cloudinary.com/gonation/${coverImageId}')`,
+      }}
+    ></Flex>
   );
 }
 
