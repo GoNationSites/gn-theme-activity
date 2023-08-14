9f6c1ac9b1c745f0153af849f2f1e50f970d56f6
esmith7196 <ericsmith.100@gmail.com>
2023-08-14 11:31:35 -0400

    [skip ci] Add Caffee Italia

---
+++ caffeItalia/gatsby-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-0MAhf0IzTt_vgneq1Rz8_w',
+        poweredID: 'bzn-0MAhf0IzTt_vgneq1Rz8_w',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+
+        orderOnlineLink: '', // leave as empty string if not needed
+        orderOnlineName: '',
+      },
+    },
+  ],
+};
