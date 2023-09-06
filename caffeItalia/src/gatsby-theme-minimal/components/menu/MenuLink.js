e5f8834ddc0fd05b91bc539874ef6e3e7d91f1f3
Stefan Trinh <32978225+stefantrinh1@users.noreply.github.com>
2023-09-06 09:26:42 -0700

    [skip ci] adding changes to cafee italia]

--- caffeItalia/src/gatsby-theme-minimal/components/menu/MenuLink.js
+++ caffeItalia/src/gatsby-theme-minimal/components/menu/MenuLink.js
@@ -5,17 +5,17 @@ import { faMap } from '@fortawesome/free-solid-svg-icons';
 
 export default function MenuLink({ gonationSlug, menuOrServices }) {
   return (
-    <Box variant="menu">
+    <Box variant='menu'>
       <Link
-        variant="menu.link"
-        target="_blank"
+        variant='menu.link'
+        target='_blank'
         href={
-          'https://res.cloudinary.com/gonation/image/upload/v1692220039/caffeItalia/caffe-italia-tv-menu-2.jpg'
+          'https://res.cloudinary.com/gonation/image/upload/v1694017511/caffeItalia/caffe-italia-tv-menu-2.jpg'
         }
-        rel="noreferrer"
+        rel='noreferrer'
       >
         <FontAwesomeIcon icon={faMap} />
-        <Text variant="menu.text">See Our Menu</Text>
+        <Text variant='menu.text'>See Our Menu</Text>
       </Link>
     </Box>
   );
