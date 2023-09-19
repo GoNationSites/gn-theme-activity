5d68e05ca2439f8ad37900c808c65a539231aef6
Stefan Trinh <32978225+stefantrinh1@users.noreply.github.com>
2023-09-19 12:39:42 -0700

    [skip ci] temp page

---
+++ moltoDarien/gatsby-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-syBAtwtTR4_VECRpu0oAKg',
+        poweredID: 'bzn-syBAtwtTR4_VECRpu0oAKg',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink:
+          'https://res.cloudinary.com/gonation/image/upload/v1695152113/sites/molto-darien/job-application.pdf', // leave as empty string if not needed
+        orderOnlineName: 'Apply Here',
+      },
+    },
+  ],
+};
