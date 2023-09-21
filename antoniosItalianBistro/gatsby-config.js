52c3ddce49e1dc2e56a140a4d3b68080653b40a4
Stefan Trinh <32978225+stefantrinh1@users.noreply.github.com>
2023-09-21 07:51:59 -0700

    [skip ci] adding new temp site

---
+++ antoniosItalianBistro/gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-8lMzvtJnQCuRQxYaxC53Jw',
+        poweredID: 'bzn-8lMzvtJnQCuRQxYaxC53Jw',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: '', // leave as empty string if not needed
+        orderOnlineName: '', // leave as empty string if not needed
+      },
+    },
+  ],
+};
