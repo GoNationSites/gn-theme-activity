c2bea0736e101effc8eb9de40488f692026bb8b9
Stefan Trinh <32978225+stefantrinh1@users.noreply.github.com>
2023-09-22 09:59:25 -0700

    [skip ci] adding changes to bulla

---
+++ bulaBbq/gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-enqkUcDMQ6mbC_-MW-2DTA',
+        poweredID: 'bzn-enqkUcDMQ6mbC_-MW-2DTA',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: '', // leave as empty string if not needed
+        orderOnlineName: '', // leave as empty string if not needed
+      },
+    },
+  ],
+};
