c01ed46adaaa8ac5e3cef7e381b32849f9e7fd52
Stefan Trinh <32978225+stefantrinh1@users.noreply.github.com>
2023-09-22 07:26:51 -0700

    [skip ci] adding 123 pronto temp site

---
+++ 123Pronto/gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-P-j_T9UIQl2ructqBN-uQQ',
+        poweredID: 'bzn-P-j_T9UIQl2ructqBN-uQQ',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: '', // leave as empty string if not needed
+        orderOnlineName: '', // leave as empty string if not needed
+      },
+    },
+  ],
+};
