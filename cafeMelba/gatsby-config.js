e5294f260ec54dce32930653cd7face0c682a631
Stefan Trinh <32978225+stefantrinh1@users.noreply.github.com>
2023-10-06 08:00:06 -0700

    [skip ci] adding new temp site

---
+++ cafeMelba/gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-pAIcaH8HQ3myu3ZIIYMASQ',
+        poweredID: 'bzn-pAIcaH8HQ3myu3ZIIYMASQ',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: '', // leave as empty string if not needed
+        orderOnlineName: '', // leave as empty string if not needed
+      },
+    },
+  ],
+};
