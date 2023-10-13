4c928d80fd9665f0c0a9785a9fd24a1943c884cc
Stefan Trinh <32978225+stefantrinh1@users.noreply.github.com>
2023-10-13 08:18:43 -0700

    [skip ci] adding changes to favios

--- faviosTrattoria/src/gatsby-theme-minimal/components/menu/MenuLink.js
+++ faviosTrattoria/src/gatsby-theme-minimal/components/menu/MenuLink.js
@@ -5,14 +5,17 @@ import { faMap } from '@fortawesome/free-solid-svg-icons';
 
 export default function MenuLink({ gonationSlug, menuOrServices }) {
   console.log('menulink');
+  const characters = '0123456789';
+  const number = (Math.random() * characters.length)
+    .toString()
+    .replace('.', '');
+
   return (
     <Box variant='menu'>
       <Link
         variant='menu.link'
         target='_blank'
-        href={
-          'https://res.cloudinary.com/gonation/image/upload/v1694103269/sites/favios-trattoria/lunch-menu.pdf'
-        }
+        href={`https://res.cloudinary.com/gonation/image/upload/v${number}/sites/favios-trattoria/lunch-menu.pdf`}
         rel='noreferrer'
       >
         <FontAwesomeIcon icon={faMap} />
@@ -21,9 +24,7 @@ export default function MenuLink({ gonationSlug, menuOrServices }) {
       <Link
         variant='menu.link'
         target='_blank'
-        href={
-          'https://res.cloudinary.com/gonation/image/upload/v1694103269/sites/favios-trattoria/dinner-menu.pdf'
-        }
+        href={`https://res.cloudinary.com/gonation/image/upload/v${number}/sites/favios-trattoria/dinner-menu.pdf`}
         rel='noreferrer'
       >
         <FontAwesomeIcon icon={faMap} />
