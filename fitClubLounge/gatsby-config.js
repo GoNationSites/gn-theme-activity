b2285c2c335fa55ffbaea01446a9d31c3353d41b
Stefan Trinh <32978225+stefantrinh1@users.noreply.github.com>
2023-10-13 09:20:39 -0700

    [skip ci] adding new temp site fit club

---
+++ fitClubLounge/gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-uUNeBnhkSVm7E-XjRlpGLA',
+        poweredID: 'bzn-uUNeBnhkSVm7E-XjRlpGLA',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: '', // leave as empty string if not needed
+        orderOnlineName: '', // leave as empty string if not needed
+      },
+    },
+  ],
+};
