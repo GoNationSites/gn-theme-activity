a894adb50d58a4629b16cc6dc775be4a0c67c92d
Stefan Trinh <32978225+stefantrinh1@users.noreply.github.com>
2023-10-26 08:41:23 -0700

    [skip ci] adding new activity page

--- gatsby-theme-minimal/src/components/logo/Logo.js
+++ gatsby-theme-minimal/src/components/logo/Logo.js
@@ -1,28 +1,29 @@
-import React, { useEffect, useState, useRef } from 'react'
-import { Image } from 'theme-ui'
+import React, { useEffect, useState, useRef } from 'react';
+import { Image } from 'theme-ui';
 
 export default function Logo({ logoImageId }) {
-  const [scrolled, SetScrolled] = useState(false)
+  const [scrolled, SetScrolled] = useState(false);
 
-  const scrollRef = useRef()
-  scrollRef.current = scrolled
+  const scrollRef = useRef();
+  scrollRef.current = scrolled;
 
   useEffect(() => {
     const handleScroll = () => {
-      const show = window.scrollY > 70
+      const show = window.scrollY > 70;
       if (scrollRef.current !== show) {
-        SetScrolled(show)
+        SetScrolled(show);
       }
-    }
+    };
 
-    document.addEventListener('scroll', handleScroll)
-  }, [])
+    document.addEventListener('scroll', handleScroll);
+  }, []);
 
   return (
     <Image
       src={`https://res.cloudinary.com/gonation/${logoImageId}`}
       variant={`logo.${!scrolled ? 'logo' : 'logoScrolled'}`}
+      className={`${!scrolled ? 'logo' : 'logoScrolled'}`}
       alt='brand'
     />
-  )
+  );
 }
---
+++ superiorRoofing/gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-XGKz-6G_TfKc-3V5x0lZ1Q',
+        poweredID: 'bzn-XGKz-6G_TfKc-3V5x0lZ1Q',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: false,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: '', // leave as empty string if not needed
+        orderOnlineName: '', // leave as empty string if not needed
+      },
+    },
+  ],
+};
