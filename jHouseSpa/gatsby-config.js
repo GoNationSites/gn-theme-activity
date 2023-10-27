163e4b5674a356e13909395e8bb93174905a3678
Stefan Trinh <32978225+stefantrinh1@users.noreply.github.com>
2023-10-27 10:57:43 -0700

    [skip ci] adding j house spa

---
+++ jHouseSpa/gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-KUdw9Vq7SFqIWEmK856n-g',
+        poweredID: 'bzn-KUdw9Vq7SFqIWEmK856n-g',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: 'https://phorest.com/book/salons/thejhousespa', // leave as empty string if not needed
+        orderOnlineName: 'Bookings', // leave as empty string if not needed
+      },
+    },
+  ],
+};
