33ef45344c0e89f32ba70caccf924bcc8ae381d3
Stefan Trinh <32978225+stefantrinh1@users.noreply.github.com>
2023-11-02 09:36:11 -0700

    [skip ci] adding changes to anis table

--- anisMarket/src/gatsby-theme-minimal/components/menu/MenuLink.js
+++ anisMarket/src/gatsby-theme-minimal/components/menu/MenuLink.js
@@ -5,18 +5,42 @@ import { faMap } from '@fortawesome/free-solid-svg-icons';
 
 export default function MenuLink({ gonationSlug, menuOrServices }) {
   return (
-    <Box variant='menu' className='menu'>
-      <Link
-        variant='menu.link'
-        target='_blank'
-        href={
-          'https://res.cloudinary.com/gonation/image/upload/v1698330378/sites/anis-table/ATM_THANKSGIVING_MENU_2023.pdf'
-        }
-        rel='noreferrer'
-      >
-        <FontAwesomeIcon icon={faMap} />
-        <Text variant='menu.text'>Thanksgiving Menu</Text>
-      </Link>
-    </Box>
+    <>
+      <Box variant='menu' className='menu'>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100%',
+          }}
+        >
+          <Link
+            variant='menu.link'
+            target='_blank'
+            href={
+              'https://res.cloudinary.com/gonation/image/upload/v1698330378/sites/anis-table/ATM_THANKSGIVING_MENU_2023.pdf'
+            }
+            rel='noreferrer'
+          >
+            <FontAwesomeIcon icon={faMap} />
+            <Text variant='menu.text'>Thanksgiving Menu</Text>
+          </Link>
+
+          <Link
+            variant='menu.link'
+            target='_blank'
+            href={
+              'https://res.cloudinary.com/gonation/image/upload/v1698942705/sites/anis-table/holiday-dessert-menu.pdf'
+            }
+            rel='noreferrer'
+          >
+            <FontAwesomeIcon icon={faMap} />
+            <Text variant='menu.text'>Holiday Dessert Menu</Text>
+          </Link>
+        </Box>
+      </Box>
+    </>
   );
 }
