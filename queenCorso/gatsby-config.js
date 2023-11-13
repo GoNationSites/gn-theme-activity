a03e81a58d3f07c8d568a9efb6918b35b07a59e1
Stefan Trinh <32978225+stefantrinh1@users.noreply.github.com>
2023-11-13 06:56:48 -0800

    [skip ci] adding new site queen corso

---
+++ queenCorso/gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-minimal`,
+      options: {
+        gonationID: 'bzn-Dp6t7uoBR4Cn8anydX-0YQ',
+        poweredID: 'bzn-Dp6t7uoBR4Cn8anydX-0YQ',
+        seoKeywords: '',
+        hasAbout: true,
+        hasMenu: true,
+        hasShout: true,
+        hasHours: true,
+        hasContact: true,
+        orderOnlineLink: '', // leave as empty string if not needed
+        orderOnlineName: '',
+      },
+    },
+  ],
+};
