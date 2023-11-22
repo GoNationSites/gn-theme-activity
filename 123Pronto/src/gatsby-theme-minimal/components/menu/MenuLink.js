7a93f9fe266067159a6a6305e2c1571e6ebc2fac
Stefan Trinh <32978225+stefantrinh1@users.noreply.github.com>
2023-11-22 09:31:58 -0800

    [skip ci] adding pdf changes to 23 pronot

--- 123Pronto/src/gatsby-theme-minimal/components/menu/MenuLink.js
+++ 123Pronto/src/gatsby-theme-minimal/components/menu/MenuLink.js
@@ -5,17 +5,38 @@ import { faMap } from '@fortawesome/free-solid-svg-icons';
 
 export default function MenuLink({ gonationSlug, menuOrServices }) {
   return (
-    <Box variant='menu'>
+    <Box
+      variant='menu'
+      sx={{
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        py: '0.5rem',
+        a: {
+          border: 'solid 1px',
+          borderRadius: '10px',
+          m: '0.5rem',
+        },
+      }}
+    >
       <Link
         variant='menu.link'
         target='_blank'
         href={
-          'https://res.cloudinary.com/gonation/image/upload/v1696882473/sites/123pronto/123_Pronto_menu.pdf'
+          'https://res.cloudinary.com/gonation/image/upload/v1700673958/sites/123pronto/catering_menu.pdf'
         }
         rel='noreferrer'
       >
-        <FontAwesomeIcon icon={faMap} />
-        <Text variant='menu.text'>See Our Menu</Text>
+        <Text variant='menu.text'>Catering Menu</Text>
+      </Link>
+      <Link
+        variant='menu.link'
+        target='_blank'
+        href='https://res.cloudinary.com/gonation/image/upload/v1700674152/sites/123pronto/entree_list.pdf'
+        rel='noreferrer'
+      >
+        <Text variant='menu.text'>Entree List</Text>
       </Link>
     </Box>
   );
